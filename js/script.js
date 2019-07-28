f9e49da1df10b9dc840758b3b0269e23b6db163d
AdamBabinicz <puaro@vp.pl>
2019-07-28 17:57:35 +0200

    dodano adres bloga

--- js/script.js
+++ js/script.js
@@ -57,6 +57,9 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace("active", "");
   }
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
